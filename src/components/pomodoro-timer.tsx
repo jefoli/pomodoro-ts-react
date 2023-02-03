fe7f76ea3ef278b7bdc2bd4dfe9c0abc34688141
Jeferson Oliveira <jheferson.contato@hotmail.com>
2023-02-03 17:16:55 -0300

    criado funcionalidade do botão pause

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useInterval } from '../hooks/user-interval';
 import { Button } from './button';
 import { Timer } from './timer';
@@ -12,19 +12,36 @@ interface Props {
 
 export function PomodoroTimer(props: Props): JSX.Element {
   const [mainTime, setMainTime] = useState(props.PomodoroTime);
+  const [timeCounting, setTimeCounting] = useState(false);
+  const [working, setWorking] = useState(false);
 
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+  }, [working]);
+
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+    },
+    timeCounting ? 1000 : null,
+  );
+
+  const configWork = () => {
+    setTimeCounting(true);
+    setWorking(true);
+  };
 
   return (
     <div className="pomodoro">
       <h2>You are: working</h2>
       <Timer mainTime={mainTime} />
       <div className="controls">
+        <Button text="Work" onClick={() => configWork()}></Button>
         <Button text="teste"></Button>
-        <Button text="teste"></Button>
-        <Button text="teste"></Button>
+        <Button
+          text={timeCounting ? 'Pause' : 'Retornar'}
+          onClick={() => setTimeCounting(!timeCounting)}
+        ></Button>
       </div>
       <div className="details">
         <p>TESTANDO</p>
